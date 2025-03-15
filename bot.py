dcac7943d9280d83ae5116c2844c45a42f5dd7da
Giuseppe Desiato <giodesiato@gmail.com>
2025-03-15 11:11:13 +0100

    change model

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def generate_word():
         "Format the output as:\n\nWord: <word>\nExample: <example sentence>\nTranslation: <translation>"
     )
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content
